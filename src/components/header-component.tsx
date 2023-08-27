db55f28fb91d79e22a479ac3b3dd221ddeb45013
Feredev <hector1567xd@gmail.com>
2023-08-27 03:43:44 -0400

    Added Shop / Business

--- src/components/header-component.tsx
+++ src/components/header-component.tsx
@@ -2,16 +2,25 @@ import styledComponents from "styled-components";
 import { FunctionComponent } from "react";
 import { BiArrowBack } from "react-icons/bi";
 import { FiTarget } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 
 const BusinessPageHeader: FunctionComponent<{
   className?: string;
   labelText: string;
 }> = ({ className, labelText }) => {
+  const navigate = useNavigate();
+
   return (
     <div className={className}>
       <div className="bg-white header-business-page w-full">
         <div className="text-brand-purple-dark flex-1">
-          <BiArrowBack size={28} />
+          <BiArrowBack
+            size={28}
+            onClick={() => {
+              console.log("back");
+              navigate("/categories");
+            }}
+          />
         </div>
         <div className="text-lg font-bold text-brand-purple-dark flex-1">
           {labelText}
