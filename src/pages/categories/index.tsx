b0f920b725a6a47f78b9ddcc9dea7895e0d914d6
noralgorithm <joseandresrs0407@gmail.com>
2023-08-27 01:54:30 -0430

    feat: setted up font

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,5 +1,27 @@
+import { AiFillCrown, AiOutlineHome } from "react-icons/ai";
+
 function CategoriesPage() {
-  return <div className="text-brand-aqua">CategoriesPage</div>;
+  return (
+    <div className="w-full h-full flex flex-col font-bold">
+      <header className="bg-brand-aqua w-full pt-8 px-2">
+        <div className="flex items-center justify-between">
+          <div className="flex">
+            <AiFillCrown size={30} className="text-brand-yellow" />
+            <h2 className="text-white text-2xl">
+              Hola, Usuario
+            </h2>
+          </div>
+          <div className="flex gap-2">
+            <AiOutlineHome className="text-white" size={30} />
+            <h2 className="text-white text-lg font-thin">
+              Caracas
+            </h2>
+          </div>
+        </div>
+      </header>
+      <main className="bg-white w-full"></main>
+    </div>
+  );
 }
 
 export default CategoriesPage;
