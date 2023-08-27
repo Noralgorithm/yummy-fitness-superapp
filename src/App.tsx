0914fc5f4ed467ff18d5f26e4c6e31c00e50ad77
Feredev <hector1567xd@gmail.com>
2023-08-27 01:05:09 -0400

    merge conflcts

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
-<<<<<<< HEAD
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
 function App() {
   return <RouterProvider router={router} />;
-=======
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
->>>>>>> ef3caa4eec55f33b1194c2418b621711a37b5505
 }
 
 export default App;
